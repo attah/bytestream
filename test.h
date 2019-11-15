6cf216037520de3977d1a1147e339d24ee72ca87
Anton Thomasson <antonthomasson@gmail.com>
2019-11-15 22:01:55 +0100

    Move stream manipulators in to DebugStream

--- test.h
+++ test.h
@@ -14,25 +14,22 @@ class DebugStream
       used=true;
       return *this;
     }
+    DebugStream& operator<<(std::ostream& (*f)(std::ostream &)) {
+      f(std::cout);
+      used=true;
+      return *this;
+    }
+    DebugStream& operator<<(std::ostream& (*f)(std::ios &)) {
+      f(std::cout);
+      used=true;
+      return *this;
+    }
+    DebugStream& operator<<(std::ostream& (*f)(std::ios_base &)) {
+      f(std::cout);
+      used=true;
+      return *this;
+    }
 };
-
-DebugStream& operator<< (DebugStream &d, std::ostream& (*f)(std::ostream &)) {
-  f(std::cout);
-  return d;
-  d.used=true;
-}
-
-DebugStream& operator<< (DebugStream &d, std::ostream& (*f)(std::ios &)) {
-  f(std::cout);
-  d.used=true;
-  return d;
-}
-
-DebugStream& operator<< (DebugStream &d, std::ostream& (*f)(std::ios_base &)) {
-  f(std::cout);
-  d.used=true;
-  return d;
-}
 
 #define R(s) "\033[31m" s "\033[39m"
 #define G(s) "\033[32m" s "\033[39m"
