627fbd329f17ac7b9119ee1fd35c54f8e2bff359
Anton Thomasson <antonthomasson@gmail.com>
2023-04-01 16:36:50 +0200

    Simplify Array type

--- array.h
+++ array.h
@@ -4,15 +4,14 @@
 #include <memory>
 
 template<typename T>
-class Array : public std::unique_ptr<T, std::default_delete<T[]>>
+class Array : public std::unique_ptr<T[]>
 {
 public:
-  Array(size_t size = 0)
-  : std::unique_ptr<T, std::default_delete<T[]>>(size == 0 ? nullptr : new T[size])
+  Array(size_t size = 0) : std::unique_ptr<T[]>(size == 0 ? nullptr : new T[size])
   {}
   operator T*() const
   {
-    return std::unique_ptr<T, std::default_delete<T[]>>::get();
+    return std::unique_ptr<T[]>::get();
   }
 };
 
