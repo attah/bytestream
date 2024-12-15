8de863ee24e4113289c9800a15f291e31347a001
Anton Thomasson <antonthomasson@gmail.com>
2024-12-15 15:48:46 +0100

    Add clang-tidy, retire clang-analyze

--- bytestream.h
+++ bytestream.h
@@ -36,11 +36,11 @@ public:
   class Badmatch : public std::invalid_argument::invalid_argument
   {
   public:
-    Badmatch(std::string s, std::string v, std::string u) :
-        invalid_argument(s+": "+v+" != "+u) {}
+    Badmatch(std::string str, std::string value, std::string expected) :
+        invalid_argument(str+": "+value+" != "+expected) {}
     template<typename T>
-    Badmatch(std::string s, T v, T u) :
-        invalid_argument(s+": "+std::to_string(v)+" != "+std::to_string(u)) {}
+    Badmatch(std::string str, T value, T expected) :
+        invalid_argument(str+": "+std::to_string(value)+" != "+std::to_string(expected)) {}
 
   };
 
@@ -50,24 +50,24 @@ public:
     LittleEndian
   };
 
-  Bytestream(Endianness e = BigEndian);
-  Bytestream(size_t size, Endianness e = BigEndian);
-  Bytestream(size_t size, int pattern, Endianness e = BigEndian);
-  Bytestream(const void* data, size_t len, Endianness e = BigEndian);
-  Bytestream(const std::string& s);
-  Bytestream(std::istream& is);
-  Bytestream(std::istream& is, size_t len, Endianness e = BigEndian);
-  Bytestream(std::initializer_list<Bytes> il, Endianness e = BigEndian);
+  Bytestream(Endianness endianness = BigEndian);
+  Bytestream(size_t size, Endianness endianness = BigEndian);
+  Bytestream(size_t size, int pattern, Endianness endianness = BigEndian);
+  Bytestream(const void* data, size_t len, Endianness endianness = BigEndian);
+  Bytestream(const std::string& str);
+  Bytestream(std::istream& istream);
+  Bytestream(std::istream& istream, size_t len, Endianness endianness = BigEndian);
+  Bytestream(std::initializer_list<Bytes> bytesList, Endianness endianness = BigEndian);
 
   Bytestream(const Bytestream& rhs);
-  Bytestream(Bytestream&& rhs);
-  ~Bytestream();
+  Bytestream(Bytestream&& rhs) noexcept;
+  ~Bytestream() = default;
 
   bool operator==(const Bytestream& other) const;
   bool operator!=(const Bytestream& other) const;
 
   Bytestream& operator=(const Bytestream& other);
-  Bytestream& operator=(Bytestream&& other);
+  Bytestream& operator=(Bytestream&& other) noexcept;
 
   uint8_t* raw() const {return _data;}
   size_t size() const {return _size;}
@@ -76,11 +76,11 @@ public:
   bool atEnd() const {return _pos >= _size;}
   void setPos(size_t pos);
   Endianness getEndianness() const {return _endianness;}
-  void setEndianness(Endianness e) {_endianness = e;}
+  void setEndianness(Endianness endianness) {_endianness = endianness;}
   void reset();
   std::string hexdump(size_t length) const;
 
-  operator bool();
+  operator bool() const;
 
   template <typename T, FOR_FIXED_WIDTH(T)>
   T get()
@@ -93,9 +93,9 @@ public:
 
   std::string getString(size_t len);
   Bytestream getBytestream(size_t len);
-  void getBytes(void* cs, size_t len);
+  void getBytes(void* data, size_t len);
   void getBytes(Bytestream& other, size_t len);
-  void peekBytes(void* cs, size_t len) const;
+  void peekBytes(void* data, size_t len) const;
   void peekBytes(Bytestream& other, size_t len) const;
 
   template <typename T, FOR_FIXED_WIDTH(T)>
@@ -111,13 +111,13 @@ public:
   Bytestream peekBytestream(size_t len) const;
 
   template <typename T, FOR_FIXED_WIDTH(T)>
-  bool next(const T& u)
+  bool next(const T& expected)
   {
     if(remaining() < sizeof(T))
     {
       return false;
     }
-    else if(u == peek<T>())
+    else if(expected == peek<T>())
     {
       _after(sizeof(T));
       return true;
@@ -128,64 +128,64 @@ public:
     }
   }
 
-  bool nextString(const std::string& bts, bool compareEqual=true);
+  bool nextString(const std::string& str, bool compareEqual=true);
   bool nextBytestream(const Bytestream& bts, bool compareEqual=true);
 
   template <typename T, FOR_FIXED_WIDTH(T)>
-  void put(T u)
+  void put(T value)
   {
-    maybeByteSwap(u);
-    putBytes(&u, sizeof(T));
+    maybeByteSwap(value);
+    putBytes(&value, sizeof(T));
   }
 
   void putString(const std::string&);
   void putBytestream(const Bytestream&);
-  void putBytes(const void* c, size_t len);
+  void putBytes(const void* data, size_t len);
 
-  Bytestream operator[](size_t i);
-  Bytestream& operator+=(size_t i);
-  Bytestream& operator-=(size_t i);
+  Bytestream operator[](size_t offset);
+  Bytestream& operator+=(size_t offset);
+  Bytestream& operator-=(size_t offset);
 
   template <typename T, FOR_FIXED_WIDTH(T)>
-  Bytestream& operator<<(T u)
+  Bytestream& operator<<(T value)
   {
-    put<T>(u);
+    put<T>(value);
     return *this;
   }
-  Bytestream& operator<<(const std::string& s);
+  Bytestream& operator<<(const std::string& str);
   Bytestream& operator<<(const Bytestream& other);
 
   template <typename T, FOR_FIXED_WIDTH(T)>
-  Bytestream& operator>>(T& u)
+  Bytestream& operator>>(T& value)
   {
-    u = get<T>();
+    value = get<T>();
     return *this;
   }
-  Bytestream& operator>>(std::string& s);
+  Bytestream& operator>>(std::string& str);
   Bytestream& operator>>(Bytestream& other);
 
   template <typename T, FOR_FIXED_WIDTH(T)>
-  Bytestream& operator>>(const T& u)
+  Bytestream& operator>>(const T& expected)
   {
-    T v = get<T>();
-    if(u!=v)
+    T value = get<T>();
+    if(expected!=value)
     {
       (*this) -= sizeof(T);
-      throw Badmatch("Does not match const", v, u);
+      throw Badmatch("Does not match const", value, expected);
     }
     else
     {
       return *this;
     }
   }
-  Bytestream& operator>>(const std::string& s);
+  Bytestream& operator>>(const std::string& str);
 
   template <typename T, FOR_FIXED_WIDTH(T)>
-  bool operator>>=(const T& u)
+  bool operator>>=(const T& expected)
   {
-    return next<T>(u);
+    return next<T>(expected);
   }
-  bool operator>>=(const std::string& s);
+  bool operator>>=(const std::string& str);
   bool operator>>=(const Bytestream& other);
 
   void putPattern(size_t len, uint8_t pattern);
@@ -210,7 +210,7 @@ private:
   void putBytes(const Bytes& b);
 
   template <typename T>
-  void maybeByteSwap(T& u) const
+  void maybeByteSwap(T& value) const
   {
     #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
     bool needsSwap = _endianness == Endianness::BigEndian;
@@ -221,7 +221,7 @@ private:
     #endif
     if(needsSwap)
     {
-      uint8_t* const p = reinterpret_cast<uint8_t*>(&u);
+      uint8_t* const p = reinterpret_cast<uint8_t*>(&value);
       for(size_t i = 0; i < sizeof(T) / 2; i++)
       {
         std::swap(p[i], p[sizeof(T) - i - 1]);
